f9a985d9fe2d3d79bccb4cad18352cf4b41da31e
Saugat Pahari <paharisaugat17@gmail.com>
2021-11-23 12:08:41 +0545

    neeeeeewww

--- resources/views/view_price.blade.php
+++ resources/views/view_price.blade.php
@@ -12,8 +12,8 @@
         <!-- Styles -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
         <style>
           
         </style>
